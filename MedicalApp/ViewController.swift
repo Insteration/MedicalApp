6791e0f1e0139e43df972b7e0c6f6ca36159ccf0
Artem <inste@icloud.com>
2019-07-29 20:53:39 +0300

    Initial Commit

---
+++ MedicalApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MedicalApp
+//
+//  Created by Артём Кармазь on 7/29/19.
+//  Copyright © 2019 iOS Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
