7034456e50b342edc23dcd74ad8fd64a6c7f5b5a
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-07-30 10:42:29 +0300

    create signIn xib with constraints

---
+++ MedicalApp/Controllers/SiggINViewControllerXIB.swift
@@ -0,0 +1,30 @@
+//
+//  SiggINViewControllerXIB.swift
+//  MedicalApp
+//
+//  Created by Артем on 7/30/19.
+//  Copyright © 2019 iOS Team. All rights reserved.
+//
+
+import UIKit
+
+class SiggINViewControllerXIB: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
