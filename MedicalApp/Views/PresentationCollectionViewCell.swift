6bc48feaed7cb800a63ab0a0d0c7897caef50f1b
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-08-14 14:24:32 +0300

    work

--- MedicalApp/Views/PresentationCollectionViewCell.swift
+++ MedicalApp/Views/PresentationCollectionViewCell.swift
@@ -2,4 +2,18 @@ import UIKit
 
 class PresentationCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var presentationImageView: UIImageView!
+    @IBOutlet weak var presentationLabel: UILabel!
+    
+    var presentItem: PresentationLogo? {
+        
+        didSet {
+            presentationLabel.text = presentItem?.text
+            
+            if let image = presentItem?.image {
+                presentationImageView.image = UIImage(named: image)
+                
+            }
+        }
+    }
 }
--- MedicalApp/Views/VideoCollectionViewCell.swift
+++ MedicalApp/Views/VideoCollectionViewCell.swift
@@ -2,4 +2,17 @@ import UIKit
 
 class VideoCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var videoImageView: UIImageView!
+    @IBOutlet weak var videoLable: UILabel!
+    
+    var videoItem: VideoLogo? {
+        
+        didSet {
+            videoLable.text = videoItem?.text
+            
+            if let image = videoItem?.image {
+                videoImageView.image = UIImage(named: image)
+            }
+        }
+    }
 }
