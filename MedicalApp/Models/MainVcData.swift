caf6a9ffc8fd94e859a855b702360c8db65c7434
holodov <holodov@holodov.dp.ua>
2019-08-15 18:54:46 +0300

    master2

---
+++ MedicalApp/Models/MainVcData.swift
@@ -0,0 +1,19 @@
+//
+//  MainVcData.swift
+//  MedicalApp
+//
+//  Created by Alex Kholodoff on 8/15/19.
+//  Copyright © 2019 iOS Team. All rights reserved.
+//
+
+//import Foundation
+
+struct PresentationLogo {
+    var image: String?
+    var text: String?
+}
+
+struct VideoLogo {
+    var image: String?
+    var text: String?
+}
