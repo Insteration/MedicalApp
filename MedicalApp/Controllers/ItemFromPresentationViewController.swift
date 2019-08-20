d6b0a8d07da340738ff2a9f25bbf44421b9bd967
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-08-20 20:18:33 +0300

    work

---
+++ MedicalApp/Controllers/ItemFromPresentationViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class ItemFromPresentationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
---
+++ MedicalApp/Controllers/ItemFromVideoViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class ItemFromVideoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
