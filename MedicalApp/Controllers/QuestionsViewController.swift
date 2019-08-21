40eb2cf3e1ab26d0834dfe3d5cb8a4f182c1e9b1
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-08-21 19:22:38 +0300

    add questions with animate

--- MedicalApp/Controllers/QuestionsViewController.swift
+++ MedicalApp/Controllers/QuestionsViewController.swift
@@ -4,17 +4,43 @@ class QuestionsViewController: UIViewController {
     
     @IBOutlet weak var questionsView: UIView!
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         questionsView.layer.cornerRadius = 25
+        self.view.backgroundColor = UIColor.black.withAlphaComponent(0.75)
+        
+        moveIn()
     }
     
     
     @IBAction func sendQuestionsActionButton(_ sender: UIButton) {
         
-        self.view.removeFromSuperview()
+        //        self.view.removeFromSuperview()
+        moveOut()
     }
     
+    
+    
+    func moveIn() {
+        
+        self.view.transform = CGAffineTransform(scaleX: 1.35, y: 1.35)
+        self.view.alpha = 0.0
+        
+        UIView.animate(withDuration: 0.24) {
+            self.view.alpha = 1.0
+        }
+    }
+    
+    func moveOut() {
+        
+        UIView.animate(withDuration: 0.24, animations: {
+            self.view.transform = CGAffineTransform(scaleX: 1.35, y: 1.35)
+            self.view.alpha = 0.0
+        }) { _ in
+            self.view.removeFromSuperview()
+        }
+        
+    }
 }
