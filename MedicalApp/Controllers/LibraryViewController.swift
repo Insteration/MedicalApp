6a328b8f3c3e63c67cde78b617199b648c6cffde
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-08-29 19:24:22 +0300

    fix

--- MedicalApp/Controllers/LibraryViewController.swift
+++ MedicalApp/Controllers/LibraryViewController.swift
@@ -4,17 +4,13 @@ import UIKit
 class LibraryViewController: UIViewController, ParserProtocol {
     
     @IBOutlet weak var libraryTextView: UITextView!
-    
-    var parseSoap = ParserSoup()
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         
 //        let answer = parse(pathFile: "file:///Users/artem/Desktop/courses/Swift/step/team%20git/MedicalApp/MedicalApp/Resources/slide_example/Back.html")
         
         let answer1 = parse(pathFile: "file:///Users/artem/Desktop/courses/Swift/step/team%20git/MedicalApp/MedicalApp/Resources/slide_example/Back.html", cssSelector: "div")
-        
-
         
         libraryTextView.text = answer1
     }
