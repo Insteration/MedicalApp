eddf8c0068789747e9dc465cea8dd72215fbd17c
holodov <holodov@holodov.dp.ua>
2019-09-04 17:43:34 +0300

    + апдейт тхт

--- MedicalApp/Controllers/LibraryViewController.swift
+++ MedicalApp/Controllers/LibraryViewController.swift
@@ -15,17 +15,20 @@ class LibraryViewController: UIViewController, ParserProtocol {
         
 //        let answer1 = parse(pathFile: "file:///Users/artem/Desktop/courses/Swift/step/team%20git/MedicalApp/MedicalApp/Resources/slide_example/Back.html", cssSelector: "div")
         
-        let html = db.getHTML()
-        let parseRString = parse(htmlString: html, cssSelector: "div")
+//        let html = db.getHTML()
+//        let parseRString = parse(htmlString: html, cssSelector: "div")
 //        let parseRString = parse(htmlString: html, cssSelector: "title")
         
 //        libraryTextView.text = html
-        libraryTextView.text = parseRString
+//        libraryTextView.text = parseRString
         
-        libraryWebView.loadHTMLString(html, baseURL: nil)
+//        libraryWebView.loadHTMLString(html, baseURL: nil)
         
 //        libraryWebView.load(URLRequest(url: URL(string: "file:///Users/alexkholodoff/Developer/MedicalApp/MedicalApp/MedicalApp/Resources/slide_example/Core.html")!))
         
-
+        let path = db.openDB()
+        db.updateTXT()
+        db.closeDB()
+        print(path)
     }
 }
--- MedicalApp/Helpers/SanteData.swift
+++ MedicalApp/Helpers/SanteData.swift
@@ -1,17 +1,24 @@
 import Foundation
 import SQLite3
 
-struct DB {
+struct DB :  ParserProtocol {
     
-    func getHTML(_ id: Int = 3) -> String {
-        guard let path = Bundle.main.path(forResource: "Sante", ofType: "db") else {return "FIG_VAM"}
-  
-        var db: OpaquePointer? = nil
+    var db: OpaquePointer? = nil
+    
+    mutating func openDB() -> String {
+        guard let path = Bundle.main.path(forResource: "Sante", ofType: "db") else {
+            return "FIG_VAM"
+        }
+        
         guard sqlite3_open(path, &db) == SQLITE_OK else {
             print("error creating DB \(Error.self)")
-            return "" }
+            return "error open DB on path =  \(path)"}
         print("create DataBase done \(path)")
         
+        return "create DataBase done \(path)"
+    }
+    
+    func getHTML(_ id: Int = 3) -> String {
         
         var values = String()
         var str: OpaquePointer? = nil
@@ -30,15 +37,86 @@ struct DB {
             print("error get HTML from DataBase")
         }
         
-        sqlite3_close(db)
         return values
     }
     
     // update slides SET txt = "parser html" WHERE id =1;
     func updateTXT(){
         
+        var html = String()
+        var str: OpaquePointer? = nil
+        var id = 3
+        
+        let query = "SELECT html FROM slides WHERE id = \(id)"
+        
+        if sqlite3_prepare_v2(db, query, -1, &str, nil) == SQLITE_OK {
+            print("query \(query) is DONE")
+        } else {
+            print("query \(query) is uncorrect")
+        }
+        
+        if sqlite3_step(str) == SQLITE_ROW {
+            html = String(cString: sqlite3_column_text(str, 0))
+        } else {
+            print("error get HTML from DataBase")
+        }
+        
+        // parser html and update field txt
+        let txt = parse(htmlString: html)
+        
+        //        updateTXT(db: db!, inTable: "slides", txt: txt, id: id)
+        updateTXT(inTable: "slides", txt: txt, id: id)
+        
+        
+        
+        // get TXT from BD
+        let queryTXT = "SELECT txt FROM slides WHERE id = 3"
+        var txtSlides = ""
+        
+        if sqlite3_prepare_v2(db, queryTXT, -1, &str, nil) == SQLITE_OK {
+            print("queryTXT \(query) is DONE")
+        } else {
+            print("queryTXT \(query) is uncorrect")
+        }
+        
+        if sqlite3_step(str) == SQLITE_ROW {
+            txtSlides = String(cString: sqlite3_column_text(str, 0))
+        } else {
+            print("error get TXT from DataBase")
+        }
+        
+        print("txtSlides = ", txtSlides)
     }
     
+    private func updateTXT(inTable: String, txt: String, id: Int) {
+        
+        var update: OpaquePointer? = nil
+        
+        print("txt = ", txt, "\nid = ", id)
+        
+        let updateString = """
+        UPDATE \(inTable) SET txt = '\(txt)' WHERE iD = \(id)
+        """
+        
+        //        let updateString = "UPDATE slides SET txt = strftime('%Y-%m-%d %H:%M:%S', datetime('now','localtime')) WHERE iD = 3"
+        
+        print("updateString = ",updateString)
+        
+        guard sqlite3_prepare_v2(db, updateString, -1, &update, nil) == SQLITE_OK,
+            sqlite3_step(update) == SQLITE_DONE else {
+                let errmsg = String(cString: sqlite3_errmsg(db)!)
+                print("error preparing update: \(errmsg)")
+                print("error run update")
+                return
+        }
+        print("update whith guard done")
+        
+        sqlite3_finalize(update)
+    }
+    
+    func closeDB() {
+        sqlite3_close(db)
+    }
     
 }
 
