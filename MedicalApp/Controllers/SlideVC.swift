493969a1dedf58f48e840e0f9afac3b39b7afca6
holodov <holodov@holodov.dp.ua>
2019-09-25 14:59:45 +0300

    SlideVC

---
+++ MedicalApp/Controllers/SlideVC.swift
@@ -0,0 +1,30 @@
+//
+//  SlideVC.swift
+//  MedicalApp
+//
+//  Created by Alex Kholodoff on 9/25/19.
+//  Copyright © 2019 iOS Team. All rights reserved.
+//
+
+import UIKit
+
+class SlideVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
