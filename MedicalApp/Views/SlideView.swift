412a977450d2a12e6050f4bcd73aa3bf527d65b0
holodov <holodov@holodov.dp.ua>
2019-10-04 23:03:18 +0300

    fix bug with label in slide view

--- MedicalApp/Views/SlideView.swift
+++ MedicalApp/Views/SlideView.swift
@@ -14,24 +14,28 @@ class SlideView: UIView {
     
     var labelName = UILabel()
     var webView = WKWebView()
-
-    init(frame: CGRect, name: String, txtHTML: String, baseUrl: URL) {
+    
+    init(frame frameSlide: CGRect, name: String, txtHTML: String, baseUrl: URL) {
         
         // TODO: - all write in super init
-        super.init(frame: frame)
-        print("super.init(frame: frame) =", frame)
+        super.init(frame: frameSlide)
+        print("super.init(frame: frame) =", frameSlide)
         
-        self.labelName.frame = CGRect(x: 0, y: 0, width: frame.width, height: 80)
-        self.labelName.backgroundColor = .lightGray
-        self.labelName.textAlignment = .center
-        self.labelName.sizeToFit()
-        self.labelName.text = name
-        print("self.labelName.frame =", self.labelName.frame)
-        
-        self.webView.frame = CGRect(x: 0, y: 100, width: frame.width, height: frame.height - 100)
+        self.webView.frame = CGRect(x: 0, y: 100, width: frameSlide.width, height: frameSlide.height - 100)
         self.webView.backgroundColor = .yellow
         self.webView.loadHTMLString(txtHTML, baseURL: baseUrl)
         print("self.webView.frame =", self.webView.frame )
+        
+        self.labelName = UILabel(frame: CGRect(x: 0, y: 0, width: frameSlide.width, height: 80))
+        //        self.labelName.frame = CGRect(x: 0, y: 0, width: frame.width, height: 80)
+        self.labelName.font = .boldSystemFont(ofSize: 20)
+        self.labelName.numberOfLines = 0
+        self.labelName.textColor = .green
+        self.labelName.backgroundColor = .lightGray
+        self.labelName.textAlignment = .center
+        self.labelName.text = name
+        self.labelName.sizeToFit()
+        print("self.labelName.frame =", self.labelName.frame)
         
         self.addSubview(self.labelName)
         self.addSubview(self.webView)
@@ -40,5 +44,5 @@ class SlideView: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
 }
